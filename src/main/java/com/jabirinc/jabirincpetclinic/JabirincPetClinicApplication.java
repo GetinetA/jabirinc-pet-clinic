364ee3bb222968ffbde74aca3079729defdc4158
Getinet Alewab <getinetbr@yahoo.com>
2019-02-21 19:14:47 -0500

    Initial Project Add

---
+++ src/main/java/com/jabirinc/jabirincpetclinic/JabirincPetClinicApplication.java
@@ -0,0 +1,13 @@
+package com.jabirinc.jabirincpetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JabirincPetClinicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JabirincPetClinicApplication.class, args);
+    }
+
+}
