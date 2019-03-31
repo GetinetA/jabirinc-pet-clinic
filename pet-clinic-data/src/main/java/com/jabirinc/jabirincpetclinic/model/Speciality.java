043a644bbcf7e7a056bb705c1a50b1bf8b1ca401
Getinet Alewab <getinetbr@yahoo.com>
2019-03-31 14:37:45 -0400

    Added Speciality entity, added to Vet. Closes #27.

---
+++ pet-clinic-data/src/main/java/com/jabirinc/jabirincpetclinic/model/Speciality.java
@@ -0,0 +1,17 @@
+package com.jabirinc.jabirincpetclinic.model;
+
+/**
+ * Created by Getinet on 2019-03-31
+ */
+public class Speciality extends BaseEntity {
+
+    private String description;
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
--- pet-clinic-data/src/main/java/com/jabirinc/jabirincpetclinic/model/Vet.java
+++ pet-clinic-data/src/main/java/com/jabirinc/jabirincpetclinic/model/Vet.java
@@ -1,7 +1,19 @@
 package com.jabirinc.jabirincpetclinic.model;
 
+import java.util.Set;
+
 /**
  * Created by Getinet on 2019-02-25
  */
 public class Vet extends Person {
+
+    private Set<Speciality> specialities;
+
+    public Set<Speciality> getSpecialities() {
+        return specialities;
+    }
+
+    public void setSpecialities(Set<Speciality> specialities) {
+        this.specialities = specialities;
+    }
 }
