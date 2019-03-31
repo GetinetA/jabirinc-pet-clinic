df005742d8ec27391267feae8c1613461a7cfcd9
Getinet Alewab <getinetbr@yahoo.com>
2019-03-31 16:12:47 -0400

    Added PetTypes in with Bootstrap (startup). closes #32

--- pet-clinic-web/src/main/java/com/jabirinc/jabirincpetclinic/bootstrap/DataInitializer.java
+++ pet-clinic-web/src/main/java/com/jabirinc/jabirincpetclinic/bootstrap/DataInitializer.java
@@ -1,8 +1,10 @@
 package com.jabirinc.jabirincpetclinic.bootstrap;
 
 import com.jabirinc.jabirincpetclinic.model.Owner;
+import com.jabirinc.jabirincpetclinic.model.PetType;
 import com.jabirinc.jabirincpetclinic.model.Vet;
 import com.jabirinc.jabirincpetclinic.services.OwnerService;
+import com.jabirinc.jabirincpetclinic.services.PetTypeService;
 import com.jabirinc.jabirincpetclinic.services.VetService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
@@ -16,15 +18,29 @@ public class DataInitializer implements CommandLineRunner {
 
     private final OwnerService ownerService;
     private final VetService vetService;
+    private final PetTypeService petTypeService;
 
     @Autowired
-    public DataInitializer(OwnerService ownerService, VetService vetService) {
+    public DataInitializer(OwnerService ownerService,
+                           VetService vetService,
+                           PetTypeService petTypeService) {
         this.ownerService = ownerService;
         this.vetService = vetService;
+        this.petTypeService = petTypeService;
     }
 
     @Override
     public void run(String... args) throws Exception {
+
+        PetType dog = new PetType();
+        dog.setName("Dog");
+        PetType savedDogPetType = petTypeService.save(dog);
+
+        PetType cat = new PetType();
+        cat.setName("Cat");
+        PetType savedCatPetType = petTypeService.save(cat);
+
+        System.out.println("Loaded PetTypes....");
 
         Owner owner1 = new Owner();
         owner1.setFirstName("Michael");
