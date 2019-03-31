9f4832704108b6c7947a8ff7b8e1302224d59660
Getinet Alewab <getinetbr@yahoo.com>
2019-03-31 14:03:26 -0400

    Added Visit entity, updated Owner with Pet relationship. Closes #26.

--- pet-clinic-data/src/main/java/com/jabirinc/jabirincpetclinic/model/Owner.java
+++ pet-clinic-data/src/main/java/com/jabirinc/jabirincpetclinic/model/Owner.java
@@ -1,7 +1,19 @@
 package com.jabirinc.jabirincpetclinic.model;
 
+import java.util.Set;
+
 /**
  * Created by Getinet on 2019-02-25
  */
 public class Owner extends Person {
+
+    private Set<Pet> pets;
+
+    public Set<Pet> getPets() {
+        return pets;
+    }
+
+    public void setPets(Set<Pet> pets) {
+        this.pets = pets;
+    }
 }
