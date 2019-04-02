271444272ee980fee8fae199b06c2344bd48a9c6
Getinet Alewab <getinetbr@yahoo.com>
2019-04-02 04:06:43 -0400

    Added Specialty Map Service Implementation. Closes #31

---
+++ pet-clinic-data/src/main/java/com/jabirinc/jabirincpetclinic/services/SpecialityService.java
@@ -0,0 +1,9 @@
+package com.jabirinc.jabirincpetclinic.services;
+
+import com.jabirinc.jabirincpetclinic.model.Speciality;
+
+/**
+ * Created by Getinet on 2019-04-02
+ */
+public interface SpecialityService extends CrudService<Speciality, Long> {
+}
---
+++ pet-clinic-data/src/main/java/com/jabirinc/jabirincpetclinic/services/map/SpecialityServiceMap.java
@@ -0,0 +1,37 @@
+package com.jabirinc.jabirincpetclinic.services.map;
+
+import com.jabirinc.jabirincpetclinic.model.Speciality;
+import com.jabirinc.jabirincpetclinic.services.SpecialityService;
+
+import java.util.Set;
+
+/**
+ * Created by Getinet on 2019-04-02
+ */
+public class SpecialityServiceMap extends AbstractMapService<Speciality, Long> implements SpecialityService {
+
+    @Override
+    public Set<Speciality> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public Speciality findById(Long aLong) {
+        return super.findById(aLong);
+    }
+
+    @Override
+    public Speciality save(Speciality type) {
+        return super.save(type);
+    }
+
+    @Override
+    public void delete(Speciality object) {
+        super.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long aLong) {
+        super.deleteById(aLong);
+    }
+}
