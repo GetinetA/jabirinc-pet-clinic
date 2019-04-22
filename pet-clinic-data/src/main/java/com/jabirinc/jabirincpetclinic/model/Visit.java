557b3a383f4b48c5e2990f2490d6f84be1e1c58d
Getinet Alewab <getinetbr@yahoo.com>
2019-04-21 22:12:12 -0400

    Create Visit Entity. Closes #38

--- pet-clinic-data/src/main/java/com/jabirinc/jabirincpetclinic/model/Visit.java
+++ pet-clinic-data/src/main/java/com/jabirinc/jabirincpetclinic/model/Visit.java
@@ -1,14 +1,23 @@
 package com.jabirinc.jabirincpetclinic.model;
 
+import javax.persistence.*;
 import java.time.LocalDate;
 
 /**
  * Created by Getinet on 2019-03-31
  */
+@Entity
+@Table(name = "visits")
 public class Visit extends BaseEntity {
 
+    @Column(name = "date")
     private LocalDate date;
+
+    @Column(name = "description")
     private String description;
+
+    @ManyToOne
+    @JoinColumn(name = "pet_id")
     private Pet pet;
 
     public LocalDate getDate() {
