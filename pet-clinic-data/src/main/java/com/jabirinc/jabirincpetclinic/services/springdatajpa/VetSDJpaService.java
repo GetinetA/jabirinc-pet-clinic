49b6b8b87aa8a113f1d8014a9c0d7fff4d023a80
Getinet Alewab <getinetbr@yahoo.com>
2019-04-29 21:50:28 -0400

    Create Spring Data JPA Service for Vets. Closes #41

---
+++ pet-clinic-data/src/main/java/com/jabirinc/jabirincpetclinic/services/springdatajpa/VetSDJpaService.java
@@ -0,0 +1,55 @@
+package com.jabirinc.jabirincpetclinic.services.springdatajpa;
+
+import com.jabirinc.jabirincpetclinic.model.Vet;
+import com.jabirinc.jabirincpetclinic.repositories.VetRepository;
+import com.jabirinc.jabirincpetclinic.services.VetService;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * Created by Getinet on 2019-04-29
+ */
+@Service
+@Profile("springdatajpa")
+public class VetSDJpaService implements VetService {
+
+    private final VetRepository vetRepository;
+
+    public VetSDJpaService(VetRepository vetRepository) {
+        this.vetRepository = vetRepository;
+    }
+
+
+    @Override
+    public Set<Vet> findAll() {
+
+        Set<Vet> vets = new HashSet<>();
+
+        vetRepository.findAll().forEach(vets::add);
+
+        return vets;
+    }
+
+    @Override
+    public Vet findById(Long aLong) {
+        return vetRepository.findById(aLong).orElse(null);
+    }
+
+    @Override
+    public Vet save(Vet type) {
+        return vetRepository.save(type);
+    }
+
+    @Override
+    public void delete(Vet object) {
+        vetRepository.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long aLong) {
+        vetRepository.deleteById(aLong);
+    }
+}
