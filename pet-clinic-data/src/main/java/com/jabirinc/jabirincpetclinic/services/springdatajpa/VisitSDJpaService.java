16a10c44e783a50de3bf28b4d4d374f11f41f91c
Getinet Alewab <getinetbr@yahoo.com>
2019-05-04 04:57:17 -0400

    Implement Spring Data JPA service for Visit. Closes #47

---
+++ pet-clinic-data/src/main/java/com/jabirinc/jabirincpetclinic/services/springdatajpa/VisitSDJpaService.java
@@ -0,0 +1,52 @@
+package com.jabirinc.jabirincpetclinic.services.springdatajpa;
+
+import com.jabirinc.jabirincpetclinic.model.Visit;
+import com.jabirinc.jabirincpetclinic.repositories.VisitRepository;
+import com.jabirinc.jabirincpetclinic.services.VisitService;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * Created by Getinet on 2019-05-04
+ */
+@Service
+@Profile("springdatajpa")
+public class VisitSDJpaService implements VisitService {
+
+    private final VisitRepository visitRepository;
+
+    public VisitSDJpaService(VisitRepository visitRepository) {
+        this.visitRepository = visitRepository;
+    }
+
+    @Override
+    public Set<Visit> findAll() {
+
+        Set<Visit> visits = new HashSet<>();
+        visitRepository.findAll().forEach(visits::add);
+        return visits;
+    }
+
+    @Override
+    public Visit findById(Long aLong) {
+        return visitRepository.findById(aLong).orElse(null);
+    }
+
+    @Override
+    public Visit save(Visit type) {
+        return visitRepository.save(type);
+    }
+
+    @Override
+    public void delete(Visit object) {
+        visitRepository.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long aLong) {
+        visitRepository.deleteById(aLong);
+    }
+}
