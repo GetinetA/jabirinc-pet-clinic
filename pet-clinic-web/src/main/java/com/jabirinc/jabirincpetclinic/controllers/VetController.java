6f6794403196843dcc9f4a3607dbfdad1f66f950
Getinet Alewab <getinetbr@yahoo.com>
2020-01-02 09:54:45 -0500

    Implement Vets JSON Endpoint, closes #83.

--- pet-clinic-web/src/main/java/com/jabirinc/jabirincpetclinic/controllers/VetController.java
+++ pet-clinic-web/src/main/java/com/jabirinc/jabirincpetclinic/controllers/VetController.java
@@ -1,10 +1,15 @@
 package com.jabirinc.jabirincpetclinic.controllers;
 
+import com.jabirinc.jabirincpetclinic.model.Vet;
 import com.jabirinc.jabirincpetclinic.services.VetService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.Set;
 
 /**
  * Created by Getinet on 2019-02-28
@@ -25,5 +30,11 @@ public class VetController {
         model.addAttribute("vets", vetService.findAll());
 
         return "vets/index";
+    }
+
+    @GetMapping("/api/vets")
+    public @ResponseBody Set<Vet> getVetsJson(){
+
+        return vetService.findAll();
     }
 }
